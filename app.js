c913a9aea357b71abe00d9f204b54919398dc476
Prajal190904 <pranjalvyas45@gmail.com>
2024-05-15 19:00:51 +0530

    cors config

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const connectDB = require('./config/db.config');
 const trendsRoutes = require('./routes/cyber_secure.route') ;
 const authRoutes = require('./routes/auth.route') ;
 const adminRoutes = require('./routes/admin.route'); 
+const handleCors = require("./config/cors.config");
 const cookieParser = require('cookie-parser') ;
 
 app.set('view engine', 'ejs') ;
 app.use(cookieParser()) ;
 app.use(express.json()) ;
 app.use(express.urlencoded({ extended: true })) ;
+app.use(handleCors);
 app.use('/v1', trendsRoutes) ;
 app.use('/v1/auth', authRoutes) ;
 app.use('/v1/admin', adminRoutes) ;
